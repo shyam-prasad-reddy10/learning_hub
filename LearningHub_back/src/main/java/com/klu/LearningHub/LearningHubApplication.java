6db311dded377cc0a39b6b6173091dae226db5d1
shyam-prasad-reddy10 <shyamprasad2004@gmail.com>
2025-09-22 22:45:54 +0530

    First commit

---
+++ LearningHub_back/src/main/java/com/klu/LearningHub/LearningHubApplication.java
@@ -0,0 +1,13 @@
+package com.klu.LearningHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningHubApplication.class, args);
+		System.out.println("Welcome to Learning Hub");
+		}
+}
