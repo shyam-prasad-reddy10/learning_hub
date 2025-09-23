489380b56607dfc66a5d1aba87fe8bea2b1fe3fd
shyam-prasad-reddy10 <shyamprasad2004@gmail.com>
2025-09-23 11:08:27 +0530

    -c7

--- LearningHub_back/src/main/java/com/klu/LearningHub/LearningHubApplication.java
+++ LearningHub_back/src/main/java/com/klu/LearningHub/LearningHubApplication.java
@@ -2,12 +2,19 @@ package com.klu.LearningHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearningHubApplication {
+public class LearningHubApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearningHubApplication.class, args);
-		System.out.println("Welcome to Learning Hub");
-		}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LearningHubApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningHubApplication.class, args);
+        System.out.println("Welcome to Learning Hub");
+    }
 }
