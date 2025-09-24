c1b7d78b45b9576d6632151fd005efa754d020a3
Kamasani Shyam Prasad Reddy <shyamprasad2004@gmail.com>
2025-09-24 06:43:07 +0530

    Update App.js

--- LearningHub_front/src/App.js
+++ LearningHub_front/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Public Pages
 import HomePage from "./Component/HomePage";
